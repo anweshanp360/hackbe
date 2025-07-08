80c6ad85fa2da46821649696d643855534e6c30a
anweshanp360 <159458165+anweshanp360@users.noreply.github.com>
2025-07-08 03:39:54 -0400

    Delete Node directory

--- Node/src/routes/patient.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const patientController = require('../controller/patient.controller');
-
-router.get('/', patientController.getPatients);
-router.post('/', patientController.postPatients);
-
-module.exports = router;
